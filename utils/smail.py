d1931a04d44b307fdfe6895695b2775c89d9c844
jk-aneirin <aeiouxu@163.com>
2016-10-17 23:37:28 -0400

    use envvariables instead of hard code

--- utils/smail.py
+++ utils/smail.py
@@ -1,10 +1,11 @@
 import smtplib
 from email.mime.text import MIMEText
+import os
 
 class SendMail():
     def __init__(self):
-        self._user = '38827014@qq.com'
-        self._pwd = 'axqkbtfyhmzqbgdb'
+        self._user = os.getenv('FROMUSER')
+        self._pwd = os.getenv('SMTPPWD')
         self.port = 465
         self.subject = 'Verification Mail'
     def sendm(self,to,content):
@@ -21,6 +22,6 @@ class SendMail():
             print "Failed,%s"%e
 if __name__=='__main__':
     sm=SendMail()
-    sm.sendm('xautxuliang@163.com','url you must click')
+    sm.sendm(os.getenv('TOUSER'),'url you must click')
 
 
